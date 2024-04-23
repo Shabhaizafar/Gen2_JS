572d756b89363065b8603bc10588521b260e749d
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-23 09:59:36 +0530

    String

---
+++ String/String.js
@@ -0,0 +1,67 @@
+// "use strict";
+// fname = "Zafar";
+// console.log(fname);
+// var 
+// let 
+// const
+///////////////////////////////////////////
+// /String/
+// What is a String : Collection of Multiple Char. 
+            // String not an Array but it's similler an Array.
+
+// using Double Quotation
+//length   12345
+// var Str = "Royal";
+// //index    01234
+// console.log(Str);
+
+// // using Single Quotation
+// var Str2 = 'Technosoft';
+// console.log(Str2);
+
+// // using Back-tick
+// var Str3 = `p ltd`;
+// console.log(Str3);
+
+// console.log(Str[2]);
+
+/Methods of String/
+//1) length : 
+// console.log(Str.length);
+
+// console.log(Str[Str.length-1]);
+
+
+
+var str = "Royal Technosoft p ltd";
+console.log(str);
+console.log(str.length);
+
+//1) str.charAt
+                        // index
+// console.log(str.charAt(21));
+
+//2) str.charCodeAt
+                        //index
+// console.log(str.charCodeAt(0));
+
+//3) str.concat
+var str2 = "Gandhinagar"
+console.log(str.concat(".",str2));
+
+// str.endsWith
+// str.includes
+// str.indexOf
+// str.lastIndexOf
+// str.replace
+// str.slice
+// str.split
+// str.startsWith
+// str.substring
+// str.toLowerCase
+// str.toUpperCase
+// str.trim
+// str.trimEnd
+// str.trimStart
+// str.valueOf
+// str.substr
