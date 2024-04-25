e2a5ad83b0d49428375196f010c63c9409d1b171
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-25 10:07:50 +0530

    String,Operators

---
+++ Operators/Operators.js
@@ -0,0 +1,37 @@
+//Arithmatic/unary (inc/dec)
+// +,-,*,/,%,++,--
+
+var n1 = 12;
+var n2 = 3;
+// console.log("n1",n1);
+// console.log("n2",n2);
+
+// console.log("Add",n1+n2);
+// console.log("Sub",n1-n2);
+// console.log("Mul",n1*n2);
+// console.log("Div",n1/n2);
+// console.log("Mod",n1%n2);
+// console.log("Inc",n1++); // first Assign n1   then n1=n1+1
+// console.log("Inc",n1);
+// console.log("Inc",++n1);// First n1 = n1+1 then Assign n1
+
+// console.log("Dec",n2--);
+// console.log("Dec",n2);
+// console.log("dec",--n2);
+
+//Assignment 
+// var n1 = 12.8;
+// var n2 = 3;
+// // // =,+=,-=,*=,/=,%=
+// // console.log("=",n1=14);  // 14 
+// // console.log("+=",n1+=n2);// 17  
+// // console.log("-=",n1-=n2);// 14 
+// // console.log("/=",n1/=n2);// 4.66666  
+// // console.log("*=",n1*=n2);//   13.9999999999
+// console.log("%=",n1%=n2);//2
+
+
+//comparission 
+// ==,!=,<=,>=,<,>,===,!== 
+//logical
+// && ,||, !
