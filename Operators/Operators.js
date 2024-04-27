87debc50c7f5bc14390502f69d9a73d8b6930658
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-27 13:06:13 +0530

    Operators

--- Operators/Operators.js
+++ Operators/Operators.js
@@ -31,7 +31,53 @@ var n2 = 3;
 // console.log("%=",n1%=n2);//2
 
 
-//comparission 
+//////////////////////////////////////////////
+//comparission  
 // ==,!=,<=,>=,<,>,===,!== 
+// var n1 = 12;
+// var n2 = "24";
+
+// // console.log(`${n1} == ${n2}`,n1==n2);//false
+// // console.log(`${n1} != ${n2}`,n1!=n2);//true
+// console.log(n1>n2);//true
+// // console.log(n1<n2);//false
+// // console.log(n1>=n2);//true  //n1>n2  n1==n2
+// // console.log(n1<=n2);//false  //n1<n2  n1==n2
+
+// console.log(`${n1} === ${n2}`,n1===n2);
+// console.log(`${n1} !== ${n2}`,n1!==n2);
+
+
+///////////////////////////////////
 //logical
-// && ,||, !
+// && ,||, !    
+
+//  1) &&    if true true  =true     else false 
+//  2) ||    if false false = false  else true
+//  3) !     true > false   false >true
+// var n1 = 12;
+// var n2 =12;
+// console.log(n1>=n2 && n1!=n2);
+
+// console.log(n1<n2 || n1==n2);
+
+
+///////////////////////////////////
+// var n1 = prompt("Enter the Value of N1 : "); //"true"
+var n2 = true;
+
+
+// console.log(n1==n2);
+// console.log(n1===n2);
+
+// type casting  
+
+//number 
+// var n1 = +prompt("Enter the Value of N1 : "); //number
+// var n1 = Number(prompt("Enter the Value of N1 : "));  //number
+var n1 = Boolean(prompt("Enter the Value of N1 : "));  //boolean
+
+console.log(n1==n2);
+console.log(n1===n2);
+
+// https://quizizz.com/admin/quiz/5dd667a07b5590001ff67297/javascript-operators
