5015b2152959ff910e2121e7881af09e8a14e3ea
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-30 10:00:32 +0530

    new

--- if-else/if.js
+++ if-else/if.js
@@ -104,3 +104,9 @@
 // 4. Write a JavaScript conditional statement to find the largest of five numbers. Display an alert box to show the results.
 // Sample numbers : -5, -2, -6, 0, -1
 // Output : 0
+
+
+// *
+// * *
+// * * *
+// * * * *
