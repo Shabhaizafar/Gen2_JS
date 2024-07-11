1018040e60531c096f49dd5cac43ca2e1ae84f49
Shabhaizafar <zafarshabhai03.com>
2024-07-11 09:44:34 +0530

    Array2

---
+++ Array2/Array2.js
@@ -0,0 +1,78 @@
+// => Very Important Array Methods :-
+
+//    - Sort.
+// var Arr = [22,29,27,21,25,28,20];
+
+// var Arr = [11,111,1001,1000001,1100010,11112];
+// console.log("Before Sorting Array is : ",Arr);
+
+// Arr.sort((a,b)=>{
+//     // return a-b;  // lowest //acending
+//     return b-a;  // biggest //descending
+
+// });
+
+
+// var Arr = ["Zafar","Raj","Ajay","Rakesh"];
+// console.log("Before Sorting Array is : ",Arr);
+
+
+// Arr.sort((x,y)=>{
+//     return x.length-y.length;
+// });
+
+// console.log("after Sorting Array is : ",Arr);
+
+///////////////////////////////////////////////////////////////
+//    - Every.   : over all
+// var Arr = [22,29,27,21,25,28,20];
+// var ans = Arr.every((a)=>{
+//     return a>=20;
+// });
+// console.log(ans);
+
+
+///////////////////////////////////////////////////////////////
+//    - Some.   : any Single or more
+// var Arr = [22,29,27,21,25,28,20];
+// var ans = Arr.some((a)=>{
+//     return a>52;
+// });
+// console.log(ans);
+
+
+//////////////////////////////
+// / sort() Method:
+// Sorting Numbers: Write a function to sort an array of numbers in ascending order using the sort() method.
+
+// Sorting Strings: Sort an array of strings alphabetically (case-sensitive).
+
+// Custom Sorting: Sort an array of objects based on a specific property (e.g., age, name).
+
+// Reverse Sorting: Sort an array of numbers in descending order using sort().
+
+////////////////////////////
+// Check Array of Numbers: Write a function that checks if all elements in an array are numbers using the every() method.
+
+// String Length Check: Verify if all strings in an array have a length greater than 3.
+
+// Divisibility Check: Determine if all elements in an array of numbers are divisible by 2.
+
+//////////////////////////
+// Check for Even Numbers: Write a function that checks if there are any even numbers in an array using the some() method.
+
+// String Starting with Specific Letter: Determine if there is any string in an array that starts with the letter 'A'.
+
+
+
+
+
+
+
+//    - Foreach method.
+//    - Map method.
+//    - Filter.
+//    - Reduce.
+//    - Find.
+//    - Fill method.
+//    - Splice method.
