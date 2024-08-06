703a1801d07a696ee7ccf99dae8c2c9e3a2ca8a5
Shabhaizafar <zafarshabhai03.com>
2024-08-06 09:57:33 +0530

    multiple

---
+++ OOJ/Inheritance2.js
@@ -0,0 +1,33 @@
+// class Father{
+//     constructor(){
+//         this.fathername = "Kaleen Bhaiya";
+//     }
+// }
+
+// class Mother{
+//     constructor(){
+//         this.mothername = "Beena";
+//     }
+// }
+
+// class Child extends (Mother,Father){
+//     constructor(cname,lname){
+//         super();
+//         this.childname = cname;
+//         this.lastname = lname;
+//     }
+// }
+
+
+
+// const c1 = new Child("Munna","Tripathi");
+
+// console.log(c1);
+
+
+
+
+
+class All{
+    
+}
